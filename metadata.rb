35fb8246757fb3286e4466b4ba19334245a65ac9
Xabier de Zuazo <xabier@onddo.com>
2013-05-25 14:05:19 +0200

    metadata: added centos and debian support

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description      'Installs/Configures postfixadmin'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+supports 'debian'
+supports 'centos'
 supports 'ubuntu'
 
 depends 'apache2'
