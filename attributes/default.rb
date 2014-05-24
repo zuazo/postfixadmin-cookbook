4e8c36374f1e557a1fb0d4812479302ae72a855a
Xabier de Zuazo <xabier@onddo.com>
2014-05-24 16:36:30 +0200

    Default PostfixAdmin version updated to 2.3.7

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 
-default['postfixadmin']['version'] = '2.3.6'
+default['postfixadmin']['version'] = '2.3.7'
 default['postfixadmin']['url'] = "http://downloads.sourceforge.net/project/postfixadmin/postfixadmin/postfixadmin-#{node['postfixadmin']['version']}/postfixadmin-#{node['postfixadmin']['version']}.tar.gz"
-default['postfixadmin']['checksum'] = 'ea505281b6c04bda887eb4e6aa6c023b354c4ef4864aa60dcb1425942bf2af63'
+default['postfixadmin']['checksum'] = '761074e711ab618deda425dc013133b9d5968e0859bb883f10164061fd87006e'
 
 default['postfixadmin']['server_name'] = 'postfixadmin.onddo.com' # change this
 default['postfixadmin']['ssl'] = false
