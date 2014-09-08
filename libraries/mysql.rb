2d1be0d0b8cc8068f0a29e4fe5fa3a6d74d34de8
Xabier de Zuazo <xabier@onddo.com>
2014-09-09 01:19:50 +0200

    PostfixAdmin::MySQL refactored: removed code duplication and logger configured

--- libraries/mysql.rb
+++ libraries/mysql.rb
@@ -25,78 +25,78 @@ module PostfixAdmin
       require 'sequel'
     end
 
+    def setup_logging
+      return unless Chef::Config[:log_level] == :debug
+      @db.loggers << Chef::Log
+      @db.sql_log_level = :debug
+    end
+
     def connect
+      return unless @db.nil?
       @db = Sequel.mysql(
         host: @host,
         user: @user,
         password: @password,
         database: @dbname
-      )
+      ) if @db.nil?
+      setup_logging
     end
 
     def disconnect
+      return if @db.nil?
       @db.disconnect
+      @db = nil
+    end
+
+    def table_exist?(table)
+      connect # ensure the connection
+      @db.table_exists?(table)
+    end
+
+    def query_row_exist?(table, column, value)
+      connect
+      return false unless @db.table_exists?(table)
+      !@db[table].select(column).where(column => value).first.nil?
+    ensure
+      disconnect
+    end
+
+    def query_delete_row(table, column, value)
+      connect
+      return false unless @db.table_exists?(table)
+      @db[table].where(column => value).delete > 0
+    ensure
+      disconnect
     end
 
     def admin_exist?(user)
-      connect
-      return false unless @db.table_exists?('admin')
-      @db['SELECT 1 FROM admin WHERE username = ? LIMIT 1', user].count > 0
-    ensure
-      disconnect
+      query_row_exist?(:admin, :username, user)
     end
 
     def remove_domain_admins(user)
-      connect
-      return false unless @db.table_exists?('domain_admins')
-      @db['DELETE FROM domain_admins WHERE username = ?', user].delete > 0
-    ensure
-      disconnect
+      query_delete_row(:domain_admins, :username, user)
     end
 
     def remove_admin(user)
       result_da = remove_domain_admins(user)
-      connect
-      return result_da unless @db.table_exists?('admin')
-      @db['DELETE FROM admin WHERE username = ?', user].delete > 0
-    ensure
-      disconnect
+      return result_da unless table_exist?(:admin)
+      query_delete_row(:admin, :username, user)
     end
 
     def domain_exist?(domain)
-      connect
-      return false unless @db.table_exists?('domain')
-      @db['SELECT 1 FROM domain WHERE domain = ? LIMIT 1', domain].count > 0
-    ensure
-      disconnect
+      query_row_exist?(:domain, :domain, domain)
     end
 
     def mailbox_exist?(username)
-      connect
-      return false unless @db.table_exists?('mailbox')
-      @db['SELECT 1 FROM mailbox WHERE username = ? LIMIT 1', username]
-        .count > 0
-    ensure
-      disconnect
+      query_row_exist?(:mailbox, :username, username)
     end
 
     def alias_exist?(address)
-      connect
-      return false unless @db.table_exists?('alias')
-      @db['SELECT 1 FROM alias WHERE address = ? LIMIT 1', address].count > 0
-    ensure
-      disconnect
+      query_row_exist?(:alias, :address, address)
     end
 
     def alias_domain_exist?(alias_domain)
-      connect
-      return false unless @db.table_exists?('alias_domain')
-      @db[
-        'SELECT 1 FROM alias_domain WHERE alias_domain = ? LIMIT 1',
-        alias_domain
-      ].count > 0
-    ensure
-      disconnect
+      query_row_exist?(:alias_domain, :alias_domain, alias_domain)
     end
   end
 end
