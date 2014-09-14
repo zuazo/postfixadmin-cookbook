eaafe13eee63e8e5a12806313d8a5ca9f6a29137
Xabier de Zuazo <xabier@onddo.com>
2014-09-14 18:47:55 +0200

    version bumped to 1.1.0 WiP

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ license 'Apache 2.0'
 description 'Installs and configures PostfixAdmin, a web based interface used '\
             'to manage mailboxes, virtual domains and aliases.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.0'
+version '1.1.0' # WiP
 
 supports 'debian'
 supports 'centos'
