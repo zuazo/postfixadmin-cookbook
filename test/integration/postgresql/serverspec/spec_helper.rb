54250bb24483762cf0ed8ef9d08a6fd13fb7de7a
Xabier de Zuazo <xabier@onddo.com>
2014-11-09 19:47:11 +0100

    Update tests to work with Serverspec 2, includes Gemfile

--- test/integration/postgresql/serverspec/spec_helper.rb
+++ test/integration/postgresql/serverspec/spec_helper.rb
@@ -19,13 +19,5 @@
 
 require 'serverspec'
 
-include Serverspec::Helper::Ssh
-include Serverspec::Helper::DetectOS
-include Serverspec::Helper::Exec
-include Serverspec::Helper::Properties
-
-properties = {}
-properties[:os] =
-  Backend.backend_for('exec').check_os
-
-set_property properties
+# Set backend type
+set :backend, :exec
