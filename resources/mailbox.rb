c3742140dd60d9671e9953879f0613487be490e7
Xabier de Zuazo <xabier@onddo.com>
2015-04-07 07:34:43 +0200

    mailbox resource: Fix name attribute to be a string

--- resources/mailbox.rb
+++ resources/mailbox.rb
@@ -22,7 +22,7 @@ actions :create
 
 attribute :mailbox, kind_of: String, name_attribute: true
 attribute :password, kind_of: String, required: true
-attribute :name, kind_of: Integer, default: ''
+attribute :name, kind_of: String, default: ''
 attribute :active, kind_of: [TrueClass, FalseClass], default: true
 attribute :mail, kind_of: [TrueClass, FalseClass], default: false
 attribute :login_username, kind_of: String, required: true
